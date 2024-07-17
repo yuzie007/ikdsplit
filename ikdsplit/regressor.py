d385d4762e563d9d3c50468a923eefc521451307
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-07-17 16:24:08 +0200

    Accept multiple POSCAR files

--- ikdsplit/regressor.py
+++ ikdsplit/regressor.py
@@ -8,27 +8,15 @@ from ase import Atoms
 from ase.build import make_supercell
 
 
-def format_df(df):
-    for k in df.columns:
-        if df[k].dtype == object:
-            df[k] = df[k].str.pad(12)
-    return df
-
-
 def change_coordinates(
     atoms: Atoms,
-    df: pd.DataFrame,
     basis_change: np.ndarray,
     origin_shift: np.ndarray,
 ) -> tuple[Atoms, pd.DataFrame]:
     """Change the coordinate system."""
     atoms.set_scaled_positions(atoms.get_scaled_positions() - origin_shift)
     atoms = make_supercell(atoms, basis_change.T, order="atom-major")
-
-    df[["x", "y", "z"]] -= origin_shift
-    df[["x", "y", "z"]] @= basis_change.T
-
-    return atoms, df
+    return atoms
 
 
 def invert(
@@ -50,7 +38,33 @@ def invert(
     return inv_basis_change, inv_origin_shift
 
 
+def regress(
+    atoms: Atoms,
+    basis_change_last: np.ndarray,
+    origin_shift_last: np.ndarray,
+) -> Atoms:
+    """Regress `Atoms` to the original unit cell."""
+    atoms = ase.io.read("POSCAR")
+
+    fn = ""
+    while True:
+        fn = "wycksplit.yaml" if not fn else os.path.join("..", fn)
+        if not os.path.isfile(fn):
+            break
+        with open(fn, encoding="utf-8") as f:
+            d = yaml.safe_load(f)
+        basis_change, origin_shift = invert(d["rotation"], d["translation"])
+        atoms = change_coordinates(atoms, basis_change, origin_shift)
+
+    return change_coordinates(atoms, basis_change_last, origin_shift_last)
+
+
 def add_arguments(parser):
+    parser.add_argument(
+        "images",
+        nargs="+",
+        help="POSCAR files to be updated",
+    )
     parser.add_argument(
         "--rotation",
         nargs=9,
@@ -66,24 +80,10 @@ def add_arguments(parser):
 
 
 def run(args):
-    atoms = ase.io.read("POSCAR")
-    df = pd.read_csv("atoms_conventional.csv", skipinitialspace=True)
-
-    fn = ""
-    while True:
-        fn = "wycksplit.yaml" if not fn else os.path.join("..", fn)
-        if not os.path.isfile(fn):
-            break
-        with open(fn) as f:
-            d = yaml.safe_load(f)
-        basis_change, origin_shift = invert(d["rotation"], d["translation"])
-        atoms, df = change_coordinates(atoms, df, basis_change, origin_shift)
-
     basis_change = np.array(args.rotation).reshape(3, 3)
     origin_shift = np.array(args.translation)
-    atoms, df = change_coordinates(atoms, df, basis_change, origin_shift)
-
-    atoms.write("SPOSCAR_regressed", direct=True)
-
-    df = format_df(df)
-    df.to_csv("atoms_regressed.csv", float_format="%24.18f", index=False)
+    for fin in args.images:
+        print(fin)
+        atoms = ase.io.read(fin)
+        atoms = regress(atoms, basis_change, origin_shift)
+        atoms.write(f"S{fin}", direct=True)
