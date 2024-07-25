87189be68242ed4f168d3ca87fbb955f75c9e835
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-07-25 15:54:48 +0200

    Add `[[regress.transformations]]`

--- ikdsplit/regressor.py
+++ ikdsplit/regressor.py
@@ -13,6 +13,7 @@ from ase import Atoms
 from ase.build import make_supercell
 from ase.spacegroup.crystal_data import _lattice_centering
 
+from ikdsplit.spacegroup import invert, multiply
 from ikdsplit.utils import format_df
 
 
@@ -37,40 +38,6 @@ def change_coordinates(
     """Change the coordinate system."""
     atoms.set_scaled_positions(atoms.get_scaled_positions() - origin_shift)
     return make_supercell(atoms, basis_change.T, order="atom-major")
-
-
-def invert(
-    basis_change: np.ndarray,
-    origin_shift: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]:
-    """Invert a general change of the coordinate system.
-
-    Parameters
-    ----------
-    basis_change : np.ndarray
-        P
-    origin_shift : np.ndarray
-        p
-
-    """
-    inv_basis_change = np.linalg.inv(basis_change)
-    inv_origin_shift = -1.0 * inv_basis_change @ origin_shift
-    return inv_basis_change, inv_origin_shift
-
-
-def multiply(
-    op0: tuple[np.ndarray, np.ndarray],
-    op1: tuple[np.ndarray, np.ndarray],
-) -> tuple[np.ndarray, np.ndarray]:
-    """Multiply two changes of the coordinate system.
-
-    See Sec. (1.2.2.2) in ITA (2016).
-    """
-    basis_change_0, origin_shift_0 = op0
-    basis_change_1, origin_shift_1 = op1
-    basis_change = basis_change_0 @ basis_change_1
-    origin_shift = basis_change_0 @ origin_shift_1 + origin_shift_0
-    return basis_change, origin_shift
 
 
 def cumulate_coordinate_change(
