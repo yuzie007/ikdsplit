d41e003650f3277dcd210d3bb82157520ecc66ef
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-07-30 10:42:06 +0200

    Move several functions to `spacegroup.py`

--- ikdsplit/spacegroup.py
+++ ikdsplit/spacegroup.py
@@ -1,8 +1,13 @@
 """Spacegroup."""
 
+import pathlib
 import re
 
 import numpy as np
+import pandas as pd
+from ase.spacegroup import Spacegroup
+
+import ikdsplit
 
 
 def get_setting_for_origin_choice_2(space_group_number: int) -> int:
@@ -119,3 +124,105 @@ def parse_transformation(s: str) -> tuple[np.ndarray, np.ndarray]:
                 t[i] += parse_translation(q)
 
     return r, t
+
+
+def find_wyckoff(xyz0: np.ndarray, space_group_number: int) -> str:
+    """Find Wyckoff position.
+
+    Parameters
+    ----------
+    xyz0 : np.ndarray
+        Position to be checked.
+    space_group_number : int
+        Space group number.
+
+    Returns
+    -------
+    str
+        Wyckoff letter with multiplicity.
+
+    """
+    src = pathlib.Path(ikdsplit.__file__).parent / "database" / "wyckoff"
+    wyckoffs = pd.read_csv(src / f"{space_group_number:03d}.csv")
+
+    # get all symmetrically equivalent positions
+    # one of them should be equal to the representative Wyckoff coordinates
+    xyzs = expand_equivalent_positions(xyz0, space_group_number)
+
+    for d in wyckoffs.to_dict(orient="records")[::-1]:
+        # skip if #(symmetrically equivalent points) != multiplicity
+        if len(xyzs) != d["multiplicity"]:
+            continue
+        for xyz in xyzs:
+            r, t = parse_transformation(d["coordinates"])
+            xyz_refined = r @ xyz + t
+            diff = xyz - xyz_refined
+            diff -= np.rint(diff)
+            if np.allclose(diff, 0.0):
+                return str(d["multiplicity"]) + d["wyckoff_letter"]
+    raise RuntimeError(xyz0, space_group_number)
+
+
+def expand_equivalent_positions(
+    xyz0: np.ndarray,
+    space_group_number: int,
+) -> np.ndarray:
+    """Expand symmetrically equivalent positions.
+
+    Parameters
+    ----------
+    xyz0 : np.ndarray
+        Original positions.
+    space_group_number : int
+        Space group number.
+
+    Returns
+    -------
+    xyzs : np.ndarray
+        Symmetrically equivalent positions.
+
+    """
+    setting = get_setting_for_origin_choice_2(space_group_number)
+    symops = Spacegroup(space_group_number, setting).get_symop()
+    xyzs = [xyz0]
+    for r, t in symops:
+        xyz = r @ xyz0 + t
+        diff = xyz[None, :] - np.array(xyzs)
+        diff -= np.rint(diff)
+        if not np.any(np.all(np.isclose(diff, 0.0), axis=1)):
+            xyzs.append(xyz)
+    return np.array(xyzs)
+
+
+def reduce_equivalent_positions(
+    xyz0s: np.ndarray,
+    space_group_number: int,
+) -> np.ndarray:
+    """Reduce symmetrically equivalent positions.
+
+    Parameters
+    ----------
+    xyz0s : np.ndarray
+        Positions to be reduced.
+    space_group_number : int
+        Space group number.
+
+    Returns
+    -------
+    np.ndarray
+        Reduced positions.
+
+    """
+    setting = get_setting_for_origin_choice_2(space_group_number)
+    symops = Spacegroup(space_group_number, setting).get_symop()
+    xyzs = [xyz0s[0]]
+    for xyz0 in xyz0s:
+        for r, t in symops:
+            xyz = r @ xyz0 + t
+            diff = xyz[None, :] - np.array(xyzs)
+            diff -= np.rint(diff)
+            if np.any(np.all(np.isclose(diff, 0.0), axis=1)):
+                break
+        else:
+            xyzs.append(xyz0)
+    return np.array(xyzs)
