eccba221246616bce9cb03bb397cf553dd5d6a9f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-07-30 15:13:34 +0200

    Refactor criteria

--- ikdsplit/runner.py
+++ ikdsplit/runner.py
@@ -16,9 +16,7 @@ def recur_run(
     supergroup: int,
     group: int,
     level: int,
-    max_level: int,
-    min_order: int,
-    max_configurations: int,
+    criteria: dict[str, int],
 ) -> None:
     """Run each subgroup recursively."""
     print_group(group, level, end=" ")
@@ -26,7 +24,7 @@ def recur_run(
     print(f"(order: {order})", end=" ")
     ncs = count_configurations()
     print(f"({ncs} configurations)", end=" ")
-    if order < min_order or ncs > max_configurations:
+    if order < criteria["min_order"] or ncs > criteria["max_configurations"]:
         print("... skipped")
         return
     else:
@@ -42,41 +40,32 @@ def recur_run(
         d = config["sort"]
         sort_all(d["reference"])
 
-        if 0 < max_level <= level:
+        if 0 < criteria["max_level"] <= level:
             return
 
         subgroups = get_subgroups(group)
         for subgroup in subgroups:
-            recur_run(
-                config,
-                group,
-                subgroup,
-                level + 1,
-                max_level,
-                min_order,
-                max_configurations,
-            )
+            recur_run(config, group, subgroup, level + 1, criteria)
 
 
-def start(
-    max_level: int = 1,
-    min_order: int = 4,
-    max_configurations: int = 2**12,  # 4096
-) -> None:
+def get_default_criteria() -> dict[str, int]:
+    """Get default criteria."""
+    return {
+        "max_level": 1,
+        "min_order": 4,
+        "max_configurations": 2**12,  # 4096
+    }
+
+
+def start(criteria: dict[str, int]) -> None:
     """Start calculations."""
     config = make_default_config()
     config.update(parse_config())
 
+    criteria = get_default_criteria() | criteria
+
     print("run ...")
-    recur_run(
-        config,
-        None,
-        config["space_group_number"],
-        0,
-        max_level,
-        min_order,
-        max_configurations,
-    )
+    recur_run(config, None, config["space_group_number"], 0, criteria)
     print()
 
 
@@ -110,4 +99,9 @@ def add_arguments(parser: argparse.ArgumentParser) -> None:
 
 def run(args: argparse.Namespace) -> None:
     """Run."""
-    start(args.level, args.order, args.configurations)
+    criteria = {
+        "max_level": args.level,
+        "min_order": args.order,
+        "max_configurations": args.configurations,
+    }
+    start(criteria)
