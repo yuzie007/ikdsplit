61a33ec1a3a33978cd1332056b32db601e90cc5f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-07-30 14:26:59 +0200

    Add minimum order of point group

--- ikdsplit/runner.py
+++ ikdsplit/runner.py
@@ -7,6 +7,7 @@ from ikdsplit.filler import fill
 from ikdsplit.io import make_default_config, parse_config
 from ikdsplit.regressor import regress
 from ikdsplit.sorter import sort_all
+from ikdsplit.spacegroup import find_crystal_class, find_point_group_order
 from ikdsplit.utils import cd, get_subgroups, print_group
 
 
@@ -16,9 +17,15 @@ def recur_run(
     group: int,
     level: int,
     max_level: int,
+    min_order: int,
 ) -> None:
     """Run each subgroup recursively."""
-    print_group(group, level)
+    print_group(group, level, end=" ")
+    order = find_point_group_order(find_crystal_class(group))
+    if order < min_order:
+        print(f"(order: {order} ... skipped)")
+        return
+    print(f"(order: {order})")
 
     dn = pathlib.Path(f"{group:03d}")
     dn.mkdir(parents=True, exist_ok=True)
@@ -35,16 +42,23 @@ def recur_run(
 
         subgroups = get_subgroups(group)
         for subgroup in subgroups:
-            recur_run(config, group, subgroup, level + 1, max_level)
+            recur_run(config, group, subgroup, level + 1, max_level, min_order)
 
 
-def start(max_level: int = 1) -> None:
+def start(max_level: int = 1, min_order: int = 4) -> None:
     """Start calculations."""
     config = make_default_config()
     config.update(parse_config())
 
     print("run ...")
-    recur_run(config, None, config["space_group_number"], 0, max_level)
+    recur_run(
+        config,
+        None,
+        config["space_group_number"],
+        0,
+        max_level,
+        min_order,
+    )
     print()
 
 
@@ -57,8 +71,15 @@ def add_arguments(parser: argparse.ArgumentParser) -> None:
         type=int,
         help="level up to which maximal subgroups are checked",
     )
+    parser.add_argument(
+        "-o",
+        "--order",
+        default=4,
+        type=int,
+        help="minimum order of space group to be checked",
+    )
 
 
 def run(args: argparse.Namespace) -> None:
     """Run."""
-    start(args.level)
+    start(args.level, args.order)
